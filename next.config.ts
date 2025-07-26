5c289dea029c973890e6d0e8aa056741ab9b317f
AdlerPrep <adlerprep@gmail.com>
2025-07-26 17:37:44 +0900

    product listing, showing reviews

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    images: {
+        domains: ['i5.walmartimages.com', 'example.com']
+    }
+};
+
 
 const withNextIntl = createNextIntlPlugin();
 
